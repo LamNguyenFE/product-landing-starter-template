4039ec423c1848c2bc8296d6dd43a2cbcf1d3745
DESKTOP-1KV3OM4\ABC <nhulam75@gmail.com>
2020-11-27 10:25:03 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Learn bootstrap
+
+http://product-landing-starter-template.surge.sh
+
+Tutorial : on Youtube.
+
+Have not finished yet.
---
+++ js/main.js
@@ -0,0 +1,35 @@
+$('#product-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+$('#product-carousel1').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
